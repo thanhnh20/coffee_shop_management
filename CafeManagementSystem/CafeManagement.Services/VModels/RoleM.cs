2d9eb1f1eaad7023c9189b191c51fba67145b2c1
Echo2craft <justechojepar@gmail.com>
2023-02-21 20:03:18 +0700

    Fix Push Bug and Add API for Role

--- CafeManagementSystem/CafeManagement.Services/VModels/RoleM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CafeManagementSystemBackEnd.VModels
-{
-    public class RoleM
-    {
-        public int RoleId { get; set; }
-        public string Name { get; set; } = null!;
-    }
-}
